851f3f332473c99b9f4f0a5671e691aa9a32fed3
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2025-06-30 14:24:09 +0200

    init unit tests

---
+++ ToDoMethodoTestsTests/ToDoMethodoTestsTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoMethodoTestsTests.swift
+//  ToDoMethodoTestsTests
+//
+//  Created by Jacques HU on 30/06/2025.
+//
+
+import Testing
+
+struct ToDoMethodoTestsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
