ab36d555c85195259480c25b7c72ce648f95cce1
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2025-06-30 14:21:28 +0200

    initial commit

---
+++ ToDoMethodoTests/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ToDoMethodoTests
+//
+//  Created by Jacques HU on 30/06/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ToDoMethodoTests/ToDoMethodoTestsApp.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoMethodoTestsApp.swift
+//  ToDoMethodoTests
+//
+//  Created by Jacques HU on 30/06/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ToDoMethodoTestsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
